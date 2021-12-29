423b0cda679adc18db80fad942c49cc692db51ee
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2021-12-29 22:29:27 +0100

    creative responsive

--- js/script.js
+++ js/script.js
@@ -1,36 +1,50 @@
+/* RESPONSIVE NAV MENU */
+
+const navMenu = () => {
+  const navbarToggler = document.querySelector(".navbar-toggler");
+
+  const navList = document.querySelector(".nav-list");
+
+  navbarToggler.addEventListener("click", () => {
+    navList.classList.toggle("nav-active");
+    navbarToggler.classList.toggle("toggle");
+  });
+};
+
+navMenu();
 
 /* SCROLL REVEAL ANIMATIONS */
 
-ScrollReveal().reveal('.ani-left', {
-   origin:'left',
-   distance:'10rem',
-   duration: 1000,
-   easing:'ease-in'
+ScrollReveal().reveal(".ani-left", {
+  origin: "left",
+  distance: "10rem",
+  duration: 1000,
+  easing: "ease-in",
 });
 
-ScrollReveal().reveal('.ani-right', {
-    origin:'right',
-    distance:'10rem',
-    duration: 1000,
-    easing:'ease-in'
- });
+ScrollReveal().reveal(".ani-right", {
+  origin: "right",
+  distance: "10rem",
+  duration: 1000,
+  easing: "ease-in",
+});
 
- ScrollReveal().reveal('.ani-fade-up', {
-    opacity:0 ,
-    easing:'ease-in'
- });
+ScrollReveal().reveal(".ani-fade-up", {
+  opacity: 0,
+  easing: "ease-in",
+});
 
- ScrollReveal().reveal('.ani-bottom', {
-    origin:'bottom',
-    distance:'10rem',
-    duration: 1000,
-    easing:'ease-in'
- });
+ScrollReveal().reveal(".ani-bottom", {
+  origin: "bottom",
+  distance: "10rem",
+  duration: 1000,
+  easing: "ease-in",
+});
 
- ScrollReveal().reveal('.ani-delay', {
-    delay:500
- });
+ScrollReveal().reveal(".ani-delay", {
+  delay: 500,
+});
 
- ScrollReveal().reveal('.ani-delay-2', {
-    delay:1000
- });
+ScrollReveal().reveal(".ani-delay-2", {
+  delay: 1000,
+});
